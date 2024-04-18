d51d3cff9b3681490eb1453ae1b9c017b4add2d8
SkrebkovMihail <151344272+SkrebkovMihail@users.noreply.github.com>
2024-04-18 17:46:32 +0300

    SkrebkovMihail

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,53 +1,62 @@
 // Copyright 2022 NNTU-CS
+#include <cstdint>
 #include "alg.h"
 
 
 double pown(double value, uint16_t n) {
 
-    double result = 1.0;
-    for (uint16_t i = 0; i < n; i++) {
-        result *= value;
+    if (n == 0) {
+        return 1;
+    } else if (n == 1) {
+        return value;
+    } else {
+        double result = pown(value * value, n / 2);
+        return (n % 2 == 0) ? result : value * result;
     }
-    return result;
 }
 
 uint64_t fact(uint16_t n) {
 
-    uint64_t result = 1;
-    for (uint16_t i = 1; i <= n; i++) {
-        result *= i;
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        uint64_t result = 1;
+        for (uint16_t i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
-    return result;
 }
 
 double calcItem(double x, uint16_t n) {
 
-    return (pown(x, n) / fact(n));
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
 
-    double resr = 1.0;
-    for (uint64_t i = 1; i <= count; i++) {
-        resr += calcItem(x, i);
+    long double res = 0.0;
+    for (uint16_t i = 0; i <= count; i++) {
+        res += pown(x, i) / fact(i);
     }
-    return resr;
+    return res;
 }
 
 double sinn(double x, uint16_t count) {
 
-    double resd = 0;
-    for (uint64_t i = 1; i <= count; i++) {
-        resd += pown(-1, i - 1) * calcItem(x, (2 * i) - 1);
+    double sinn(double x, uint16_t count) {
+        long double res = 0.0;
+        for (uint16_t i = 0; i < count; i++) {
+            res += pown(-1, i) / fact(2 * i + 1) * pown(x, 2 * i + 1);
+        }
+        return res;
     }
-    return resd;
-}
 
-double cosn(double x, uint16_t count) {
+    double cosn(double x, uint16_t count) {
 
-    double resd = 0;
-    for (uint16_t i = 1; i <= count; i++) {
-        resd += pown(-1, i - 1) * calcItem(x, (2 * i) - 2);
+        long double res = 0.0;
+        for (uint16_t i = 0; i < count; i++) {
+            res += pown(-1, i) * pown(x, 2 * i) / fact(2 * i);
+        }
+        return res;
     }
-    return resd;
-}
