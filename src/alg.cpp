07b60432e8cd2f70a048132f7982e7ba460bc26e
SkrebkovMihail <151344272+SkrebkovMihail@users.noreply.github.com>
2024-05-18 12:59:50 +0300

    ?

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,57 +4,51 @@
 
 
 double pown(double value, uint16_t n) {
-
     if (n == 0) {
         return 1;
-    } else if (n == 1) {
-        return value;
+    }
+    if ((n & 1) == 0) {
+        double result = pown(value, n >> 1);
+        return result * result;
     } else {
-        double res = pown(value * value, n / 2);
-        return (n % 2 == 0) ? res : value * res;
+        return value * pown(value, n - 1);
     }
 }
 
 uint64_t fact(uint16_t n) {
-
-    if (n == 0 || n == 1) {
-        return 1;
-    } else {
-        uint64_t res = 1;
-        for (uint16_t i = 2; i <= n; i++) {
-            res *= i;
-        }
-        return res;
-    }
+    uint64_t result = 1;
+    for (int i = 2; i <= n; ++i)
+        result *= i;
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-
     return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
-    long double res = 0.0;
-    for (uint16_t i = 0; i <= count; i++) {
-        res += pown(x, i) / fact(i);
-    }
-    return res;
+    double result = 1;
+    for (uint16_t i = 1; i <= count; i++)
+        result += calcItem(x, i);
+    return result;
 }
 
-
-
 double sinn(double x, uint16_t count) {
-    long double res = 0.0;
-    for (uint16_t i = 0; i < count; i++) {
-        res += pown(-1, i) / fact(2 * i + 1) * pown(x, 2 * i + 1);
+    double result = x;
+    for (uint16_t i = 2; i <= count; i++) {
+        if ((i & 1) == 0)
+            result -= calcItem(x, 2 * i - 1);
+        else  result += calcItem(x, 2 * i - 1);
     }
-    return res;
-    }
+    return result;
+}
+
 double cosn(double x, uint16_t count) {
-    long double res = 0.0;
-    for (uint16_t i = 0; i < count; i++) {
-        res += pown(-1, i) * pown(x, 2 * i) / fact(2 * i);
+    double result = 1;
+    for (uint16_t i = 2; i <= count; i++) {
+        if ((i & 1) == 0)
+            result -= calcItem(x, 2 * i - 2);
+        else  result += calcItem(x, 2 * i - 2);
     }
-    return res;
+    return result;
 }
